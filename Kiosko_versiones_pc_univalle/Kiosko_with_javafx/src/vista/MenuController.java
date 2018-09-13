c94b3fb38244f0464fef7d4a8ad4ecea096d2dc0
Miguel Askar <migmaxor@hotmail.com>
2018-09-13 15:45:15 -0500

    13 de septiembre
    Se arregla ejecución múltiple, probar concurrencia.

--- Kiosko_versiones_pc_univalle/Kiosko_with_javafx/src/vista/MenuController.java
+++ Kiosko_versiones_pc_univalle/Kiosko_with_javafx/src/vista/MenuController.java
@@ -604,14 +604,14 @@ private static final int Y_MAX_RESP = 3000;
     }
     
     private void pintarLecturaECG() {
-        admin = new AdminDevice(null);
+        admin = new AdminDevice(this);
         admin.dispositivoDesconectado();
         admin.ConectarTcp();
     }
 
     private void pararLecturaECG() {
-        //admin.enviarComando("stopPressure",0);
-        admin.dispositivoDesconectado();
+        admin.enviarComando("stopPressure",0);
+        //admin.dispositivoDesconectado();
         admin.desconectarCliente();
     }
     
@@ -623,10 +623,6 @@ private static final int Y_MAX_RESP = 3000;
     }
     
     private void iniciarPresionManual() {
-        admin = new AdminDevice(this);        
-        admin.dispositivoDesconectado();
-        admin.ConectarTcp();
-        
         admin.enviarComando("manualPressure", 0);        
         admin.enviarComando("stopPressure",0);
         admin.enviarComando("startPressure",0);        
@@ -897,36 +893,120 @@ private static final int Y_MAX_RESP = 3000;
         textAFAC.setText("");
     }
     
+    //Variables necesarias para el método
+    boolean primeraVez= true;
+    
         @FXML
     void iniciarLecturaSeñales(ActionEvent event) {
          
-         if (banderaInicio) {
-                        pararLecturaECG();
-                        executor.shutdown();
-                        addToQueue=null;
-                        queueParam=null;
-                        btnIniciarSeñales.setText("Iniciar");
-                        //Guardar en la base de datos
-                        this.almacenarSenales();
+        Timer timerIniciar;
+        timerIniciar = new Timer();        
+        
+        
+        if (banderaInicio)         
+        {            
+            timerIniciar.cancel();
+            timerIniciar.purge();
+
+            executor.shutdownNow();
+
+            executor = Executors.newFixedThreadPool(2);
+
+            pararLecturaECG();
+
+
+            addToQueue=null;
+            queueParam=null;
+            btnIniciarSeñales.setText("Iniciar");
+            
+            
+            //Guardar en la base de datos
+            almacenarSenales();
                         
-                    } else {
-                        graficarECG1();
-                        graficarECG2();
-                        graficarSPO2();
-                        graficarRESP();
-                        pintarLecturaECG();
-                        
-                        executor = Executors.newFixedThreadPool(2); //Cambiar para posiblemente solucionar el otro error.
-                        addToQueue = new AddToQueue();
-                        queueParam=new QueueParametros();
-                        executor.execute(addToQueue);
-                        executor.execute(queueParam);
-                        prepareTimeline();
-                        btnIniciarSeñales.setText("Parar");
+        } else 
+        { 
+            if(primeraVez)
+            {
+               primeraVez= false;
+               TimerTask taskIniciar = new TimerTask() 
+                {
+
+                    @Override
+                    public void run()
+                    {
+                        hiloPrimeraVez();
                     }
-                    banderaInicio = !banderaInicio;
+                }; 
+                // Empezamos dentro de 0s 
+                timerIniciar.schedule(taskIniciar, 0);
+            }
+            else
+            {
+                TimerTask taskIniciar = new TimerTask() 
+                {
+
+                    @Override
+                    public void run()
+                    {
+                        hiloSegundaVez();
+                    }
+                }; 
+                // Empezamos dentro de 0s 
+                timerIniciar.schedule(taskIniciar, 0);
+            }
+            
+            
+        
+        }
+        banderaInicio = !banderaInicio;
 
     }
+    
+    private void hiloPrimeraVez()
+    {
+        graficarECG1();
+                            graficarECG2();
+                            graficarSPO2();
+                            graficarRESP();
+                            pintarLecturaECG();
+
+                            executor = Executors.newFixedThreadPool(2); //Cambiar para posiblemente solucionar el otro error.                       
+
+                            addToQueue = new AddToQueue();
+                            queueParam= new QueueParametros();
+
+                            //addToQueue.run();
+                            //queueParam.run(); //Mirar si esto soluciona el error de concurrencia.
+
+                            executor.execute(addToQueue);
+                            executor.execute(queueParam);
+                            prepareTimeline();
+                            //btnIniciarSeñales.setText("Parar");        
+    }
+    
+    private void hiloSegundaVez()
+    {
+        graficarECG1();
+                            graficarECG2();
+                            graficarSPO2();
+                            graficarRESP();
+                            pintarLecturaECG();
+
+                            //executor = Executors.newFixedThreadPool(2); //Cambiar para posiblemente solucionar el otro error.                       
+
+                            addToQueue = new AddToQueue();
+                            queueParam= new QueueParametros();
+
+                            //addToQueue.run();
+                            //queueParam.run(); //Mirar si esto soluciona el error de concurrencia.
+
+                            executor.execute(addToQueue);
+                            executor.execute(queueParam);
+                            prepareTimeline();
+                            //btnIniciarSeñales.setText("Parar");        
+    }
+    
+    
           
     public void almacenarSenales()
     {
@@ -1561,16 +1641,20 @@ private static final int Y_MAX_RESP = 3000;
                   }
                        Thread.sleep(7);
                 //System.out.println("Sistole: "+ admin.staticParameters.readPresSist()+", Diastole: "+admin.staticParameters.readPresDias()+", Media: "+admin.staticParameters.readPresMed()+", HR: "+admin.staticParameters.readSpo2Hr());
-             executor.execute(this);
+             
+              executor.execute(this); 
+            
+                
             } catch (InterruptedException ex) {
-                Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
+                //Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("Excepción controlada, ejecución terminada durante un Sleep");
             }
         }
     }
 
 private class QueueParametros implements Runnable {
         public void run() {
-try {
+        try {
                     asignarStaticParameters(admin.staticParameters.readHr(),
                     + admin.staticParameters.readResp(),
                     + admin.staticParameters.readSpo2Oxi(),
@@ -1954,27 +2038,42 @@ public void reproducirSPO2()
         admin.dispositivoDesconectado();
         admin.ConectarTcp();        
         
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        admin.enviarComando("manualPressure", 0);
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Timer timerIniciar;
+        timerIniciar = new Timer();
+
+        TimerTask taskIniciar = new TimerTask() 
+        {
+
+            @Override
+            public void run()
+            {   
+
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                admin.enviarComando("manualPressure", 0);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                admin.enviarComando("stopPressure", 0);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+                admin.enviarComando("startPressure", 0);
+            }
+        };
+        // Empezamos dentro de 10s 
+        timerIniciar.schedule(taskIniciar, 0);      
         
-        admin.enviarComando("stopPressure", 0);
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-            
-        admin.enviarComando("startPressure", 0);
-        
+        //-------------------------------------------------------------------
         
         Timer timer;
         timer = new Timer();
